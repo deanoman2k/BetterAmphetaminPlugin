520231c5489715cbca3464a2223ae3eca69b37b8
BeatPlay <marten.haala@gmail.com>
2016-10-31 17:06:04 +0100

    Added Kernel panic prevention (BETA 3)

--- threads.c
+++ threads.c
@@ -8,15 +8,22 @@ volatile int term_stubs = 0;
 int stub_thread(SceSize args, void *argp) {
 	for (;;) {if (term_stubs) sceKernelExitDeleteThread(0);}	
 }
-void pauseMainThread() {
-	sceKernelChangeThreadPriority(0, 0x0);
-	int i;
-	term_stubs = 0;
-	for (i=0;i<2;i++) {
-		SceUID thid = sceKernelCreateThread("thread", stub_thread, 0x0, 0x40000, 0, 0, NULL);
-		if (thid >= 0)
-			sceKernelStartThread(thid, 0, NULL);
-	}
+void pauseMainThread(char* titleid) {
+    sceKernelChangeThreadPriority(0, 0x0);
+    term_stubs = 0;
+    int thread_num = 2;
+    int i;
+    for (i = 0; i < WHITE_LISTS; i++)
+    {
+        if (strcmp(white_list[i], titleid) == 0) {
+            thread_num = 1; // or 0 ? Just disable pause thread feature(For PCSG00622 1 is OK)
+        }
+    }
+    for (i = 0; i < thread_num; i++) {
+        SceUID thid = sceKernelCreateThread("thread", stub_thread, 0x0, 0x10000, 0, 0, NULL);
+        if (thid >= 0)
+            sceKernelStartThread(thid, 0, NULL);
+    }
 }
 void resumeMainThread() {
 	term_stubs = 1;
